dee3eddc8ff851c07d0728afb076cc4c37cae673
Charles L <charles@new-web-sites.com>
2021-11-04 08:14:43 +0000

    4th Novmber 2021 - working on static files problems

--- nwsl/nwsl/settings/dev.py
+++ nwsl/nwsl/settings/dev.py
@@ -10,7 +10,21 @@ SECRET_KEY = '48dfxp5x$af*$#qxbxymzylq40byvypqf1w1)c=sz_u=)$#df$'
 
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS += ['django_sass',]
+BASE_DIR = '/var/wwww/nwsl'
+
+INSTALLED_APPS += [
+  'django_sass', 
+  # Added sass compling. https://terencelucasyap.com/using-sass-django/
+  'sass_processor', 
+  
+  ]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+# Django Sass
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
 
 with open("/etc/nwsl.conf") as config_file:
   site_config = json.load(config_file)
